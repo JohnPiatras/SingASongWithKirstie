83baec6b2fcc39604dc61f34e54e2e9f4fbf1960
burns <john.piatras@gmail.com>
2017-11-24 13:14:56 +0000

    Cleaned up main.js and added a few comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,13 @@
-//some globals
 
-var apikey = " AIzaSyAeBabA_TskH1rKGLOIVX5xt35PvqECuI8 "
+/**********************
+ * YoutTube functions  *
+ **********************/
 
+window.apikey = " AIzaSyAeBabA_TskH1rKGLOIVX5xt35PvqECuI8 ";
 window.ytPlayer = null;
 window.ytPlaylist = null;
-window.playerId="videoContainer";
-
-
+window.videoContainerElement = document.getElementById("videoContainer");
+window.faderElement = document.getElementById("fader");
 
 //**************************************************************
 // 2. This code loads the IFrame Player API code asynchronously.
@@ -16,23 +17,119 @@ var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 //**************************************************************
 
-
-/************************************************************
-/* This function is called once the API code is downloaded. *
- * It calls a number of initialisation functions            *
- ************************************************************/
+//Called when youtube api is ready
 function onYouTubeIframeAPIReady() {
 
+	initYouTubePlayer();
 	initVideoThumbnails();
-	initYoutTubePlayer();
 
 	var fullscreenButton = document.getElementById("fullscreenButton");
 	fullscreenButton.addEventListener('click', onFullscreenButtonClick, false);
 }
 
+function initYouTubePlayer() {
+	window.ytPlayer = new YT.Player("ytPlayer", {
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+	});
+}
+
+//Grabs a playlist from the SingASongWithKirstie channel on youtube
+//Gets video IDs and creates a series of thumbnails for display on front page
+function initVideoThumbnails() {
+	var centerContent = document.getElementById("centerContent");
+	centerContent.classList.add("hide");
+	var userid = "UCJdWFFx0gaQswqQQZHw2new";
+	var playlistid = "PL_iLoxbFb7q4J4KnGtTBhU5ZdroxO8rxx";
+	var maxnumbervideos = 50;
+
+	var playlist = JSON.parse(getJSONData("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=" + playlistid + "&key=" + window.apikey));
+	
+	
+    for (var n = 0; n < playlist.items.length; n++) {
+		if(playlist.items[n].kind === "youtube#playlistItem" & playlist.items[n].snippet.resourceId.kind === "youtube#video"){
+			var id = playlist.items[n].snippet.resourceId.videoId;
+			var videoThumbnail = document.createElement("div");
+			videoThumbnail.setAttribute("class", "videoThumbnail");
+			videoThumbnail.setAttribute("id", id);
+			
+			centerContent.appendChild(videoThumbnail);
+			
+			var thumbid = id + '-thumb';
+			var thumbnail = document.createElement("img");
+			thumbnail.setAttribute("src", playlist.items[n].snippet.thumbnails.high.url);
+			thumbnail.setAttribute("id", id + "-thumb");
+			thumbnail.setAttribute("class", "youtubeThumb");
+			thumbnail.addEventListener('click', onThumbnailClick, false);
+			videoThumbnail.appendChild(thumbnail);
+			
+			var thumbcaption = document.createElement("div");
+			thumbcaption.setAttribute("class", "thumbcaption");
+			thumbcaption.innerHTML = playlist.items[n].snippet.title;
+			videoThumbnail.appendChild(thumbcaption);
+		}
+    }
+	centerContent.classList.remove("hide");
+}
+
+function playVideo(vId) {
+	window.ytPlayer.loadVideoById(vId);
+	window.faderElement.classList.remove("hide");
+	window.videoContainerElement.classList.remove("hide");
+	window.ytPlayer.playVideo();
+}
+
+function stopVideo(){
+	window.ytPlayer.stopVideo();
+	window.videoContainerElement.classList.add("hide");
+	window.faderElement.classList.add("hide");
+}
+
+function onPlayerReady(event) {
+  //event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+	if (event.data === YT.PlayerState.ENDED) {
+		stopVideo();
+	}
+	if (event.data === YT.PlayerState.PAUSED) {
+		stopVideo();
+	}
+
+}
+
+/**********************************
+ * Handle user interaction events *
+ **********************************/
+function onThumbnailClick(event) {
+	var thumb = event.target;
+	var thumbContainer = thumb.parentNode;
+	playVideo(thumbContainer.id);
+}
+
+
+/*********************
+ * Utility functions *
+ *********************/
+ 
+//see comments here: https://stackoverflow.com/questions/30081301/getting-all-videos-of-a-channel-using-youtube-api
+//for fetching youtube channel data
+function getJSONData(yourUrl) {
+	var Httpreq = new XMLHttpRequest();
+	try {
+		Httpreq.open("GET", yourUrl, false);
+		Httpreq.send(null);
+		} catch (ex) {
+		alert(ex.message);
+	}
+	return Httpreq.responseText;
+}
+ 
 function onFullscreenButtonClick() {
 	var fullscreenElement = document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement;
-	
 	if(fullscreenElement == null) {
 		launchIntoFullscreen(document.documentElement);
 	} else {
@@ -61,128 +158,5 @@ function exitFullscreen() {
     document.webkitExitFullscreen();
   }
 }
-
-
-//see comments here: https://stackoverflow.com/questions/30081301/getting-all-videos-of-a-channel-using-youtube-api
-//for fetching youtube channel data
-function getJSONData(yourUrl) {
-	var Httpreq = new XMLHttpRequest();
-	try {
-		Httpreq.open("GET", yourUrl, false);
-		Httpreq.send(null);
-		} catch (ex) {
-		alert(ex.message);
-	}
-	return Httpreq.responseText;
-}
-
-
-function initYoutTubePlayer() {
-	window.ytPlayer = new YT.Player("ytPlayer", {
-        events: {
-          'onReady': onPlayerReady,
-          'onStateChange': onPlayerStateChange
-        }
-	});
-}
-
-/****************************************************************************
- * Iterate throw our list of video IDs, create a thumbnailContainer div and *
- * image for each and set the onClick handler.                              *
- ****************************************************************************/
-function initVideoThumbnails() {
-	var centerContent = document.getElementById("centerContent");
-	
-	var userid = "UCJdWFFx0gaQswqQQZHw2new";
-	var playlistid = "PL_iLoxbFb7q4J4KnGtTBhU5ZdroxO8rxx";
-	var maxnumbervideos = 50;
-	//var videoinfo = JSON.parse(getJSONData("https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=" + userid + "&maxResults=" + maxnumbervideos + "&key=" + apikey));
-	
-	//var videoinfo = JSON.parse(getJSONData("https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=" + userid + "&playlist?list=" + playlistid + "&maxResults=" + maxnumbervideos + "&key=" + apikey));
-
-	var playlist = JSON.parse(getJSONData("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=" + playlistid + "&key=" + apikey));
-	
-	
-    for (var n = 0; n < playlist.items.length; n++) {
-		//var id = window.ytPlaylist[n];
-		
-		if(playlist.items[n].kind === "youtube#playlistItem" & playlist.items[n].snippet.resourceId.kind === "youtube#video"){
-			
-			var id = playlist.items[n].snippet.resourceId.videoId;
-			
-			//create a videoThumbnail div
-			var videoThumbnail = document.createElement("div");
-			
-			videoThumbnail.setAttribute("class", "videoThumbnail");
-			videoThumbnail.setAttribute("id", id);
-			
-			
-			//videoThumbnail.setAttribute("style", "background-image:url(" + videos[n].snippet.thumbnails.high.url + ");background-size:contain;");
-			centerContent.appendChild(videoThumbnail);
-			
-			//create thumbnail image element
-			var thumbid = id + '-thumb';
-			var thumbnail = document.createElement("img");
-			//thumbnail.setAttribute("src", "http://i.ytimg.com/vi/" + id + "/hqdefault.jpg");
-			thumbnail.setAttribute("src", playlist.items[n].snippet.thumbnails.high.url);
-			thumbnail.setAttribute("id", id + "-thumb");
-			thumbnail.setAttribute("class", "youtubeThumb");
-			thumbnail.addEventListener('click', onThumbnailClick, false);
-			videoThumbnail.appendChild(thumbnail);
-			
-			var thumbcaption = document.createElement("div");
-			thumbcaption.setAttribute("class", "thumbcaption");
-			thumbcaption.innerHTML = playlist.items[n].snippet.title;
-			videoThumbnail.appendChild(thumbcaption);
-		}
-    }
-}
-
-
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-  //event.target.playVideo();
-}
-
-function onPlayerStateChange(event) {
-	if (event.data === YT.PlayerState.ENDED) {
-		var vidContainer = event.target.a.parentNode;
-
-		stopVideo();
-	}
-	if (event.data === YT.PlayerState.PAUSED) {
-		var vidContainer = event.target.a.parentNode;
-		event.target.stopVideo();
-		stopVideo();
-	}
-
-}
-
-function onThumbnailClick(event) {
-	
-	var thumb = event.target;
-	var thumbContainer = thumb.parentNode;
-	
-	//test code
-	playVideo(thumbContainer.id);
-
-}
-
-function playVideo(vId) {
-	var p = document.getElementById(window.playerId);
-	window.ytPlayer.loadVideoById(vId);
-	document.getElementById("fader").classList.remove("hide");
-	p.classList.remove("hide");
-	window.ytPlayer.playVideo();
-}
-
-function stopVideo(){
-	var p = document.getElementById(window.playerId);
-	window.ytPlayer.stopVideo();
-	p.classList.add("hide");
-	document.getElementById("fader").classList.add("hide");
-}
-
-
 
 
